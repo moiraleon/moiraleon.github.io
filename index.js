db0da8673c6f66125619830ffe86ea1e8ae34d18
Moira Leon <moira.leon2@gmail.com>
2021-08-12 21:22:01 -0600

    adding nodemailer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app =express();
 app.use(express.json());
 app.use(express.static('public'))
+const nodemailer = require("nodemailer");
 
 app.use('/public', express.static(path.join(__dirname, './public')))
 
@@ -41,7 +42,68 @@ app.get('/styles.css', (req, res) => {
     res.status(200).send(req.body)
 
 })
+//SETTING UP NODEMAILER
+
+// async function main() {
+
+//   let testAccount = await nodemailer.createTestAccount();
+
   
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: testAccount.user, // generated ethereal user
+//       pass: testAccount.pass, // generated ethereal password
+//     },
+//   });
+
+//   // send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+//     to: "bar@example.com, baz@example.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
+
+// main().catch(console.error);  
+
+var transporter = nodemailer.createTransport({
+  service:'gmail',
+  auth:{
+    user: 'moirasfeng@gmail.com',
+    pass: 'SoftEngPass!'
+  }
+});
+ var mailOptions ={
+   from: 'moirasfeng@gmail.com',
+   to: 'moirasfeng@gmail.com',
+   subject: 'Sending Email using Node.js',
+   text: 'test email'
+ };
+ transporter.sendMail(mailOptions, function(error,info){
+   if (error){
+     console.log(error);
+   }else{
+     console.log('Email sent: '+ info.response);
+   };
+ })
+
+
+
+
+
+
 const port = process.env.PORT||5200
 
 app.listen(port,()=>{console.log(`listening on port ${port}`)})
